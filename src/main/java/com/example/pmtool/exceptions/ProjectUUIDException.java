94ed9baa7b74a45efe4aaf76cb25cac4f770b766
Terriq <riqs07@gmail.com>
2022-01-16 14:59:44 -0500

    Custom Response Exception Handler
    Made Controller advice for custom exceptions

---
+++ src/main/java/com/example/pmtool/exceptions/ProjectUUIDException.java
@@ -0,0 +1,2 @@
+package com.example.pmtool.exceptions;public class ProjectUUIDException {
+}
---
+++ src/main/java/com/example/pmtool/exceptions/ProjectUUIDExceptionResponse.java
@@ -0,0 +1,9 @@
+package com.example.pmtool.exceptions;
+
+public class ProjectUUIDExceptionRes {
+    private String projectUUID;
+
+    public ProjectUUIDExceptionRes(String projectUUID){
+        this.projectUUID = projectUUID;
+    }
+}
