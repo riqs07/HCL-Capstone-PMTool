53981e1f32e22b8015ec3c62a9532e6c09d3b4a6
Terriq <riqs07@gmail.com>
2022-01-15 20:55:27 -0500

    Fixing git error
    That was 45 mins of messing with git. Problem solved got changes back and should be all clear

--- src/main/java/com/example/pmtool/repositories/ProjectRepository.java
+++ src/main/java/com/example/pmtool/repositories/ProjectRepository.java
@@ -1,2 +1,15 @@
-package com.example.pmtool.repositories;public interface ProjectRepository {
+package com.example.pmtool.repositories;
+
+import com.example.pmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
 }
