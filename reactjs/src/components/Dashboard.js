be349af1819aac7eeef15dd5221ccbf2837b84d9
Terriq <riqs07@gmail.com>
2022-01-20 16:28:43 -0500

    Styling Dashboard, Nav & ProjectItem

--- reactjs/src/components/Dashboard.js
+++ reactjs/src/components/Dashboard.js
@@ -4,10 +4,24 @@ import ProjectItem from './Project/ProjectItem'
 
 export default function Dashboard() {
     return (
-        <div>
-            <h1 className = "alert alert-warning"> Welcome to Dashboard!</h1>
+       
+    <div className="projects">
+    <div className="container">
+        <div className="row">
+            <div className="col-md-12">
+                <h1 className="display-4 text-center">Projects</h1>
+                <br />
+                <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                    Create a Project
+                </a>
+                <br />
+                <hr />
+                <ProjectItem/>
 
-            <ProjectItem/>
+            </div>
         </div>
+    </div>
+</div>
+
     )
 }
